e35d0ffc1741d0250dd4fd134900a9fbe368789b
Serge Labada <sergelabada@gmail.com>
2022-08-14 18:50:21 +0300

    input string autosize is added

--- app/src/main/java/com/example/lesson05androidhwcalculator/SCalculatorFragment.kt
+++ app/src/main/java/com/example/lesson05androidhwcalculator/SCalculatorFragment.kt
@@ -118,83 +118,81 @@ class SCalculatorFragment : Fragment() {
 
 class Calculator() {
     companion object {
-    fun result(string: String): String {
-        val operands = mutableListOf<String>()
-        if (string.isNotBlank()) {
-            val regex = Regex("""-?\d+(\.\d+)?[*/\-+]?""")
-            regex.findAll(string).forEach {
-                operands.add(it.value)
-            }
-        }
-        //removing the last symbol of the last operand if action
-        operands[operands.size - 1] = operands.last().replace("""[*/\-+]?$""".toRegex(), "")
-        //perform actions:
-        var i = 0
-        var size = operands.size
-        //first multiply and divide
-        while (i < (size - 1)) {
-            val num1 = operands[i].dropLast(1).toDouble()
-            var num2: Double
-            var act2: String
-            if (i == size - 2) {
-                num2 = operands[i + 1].toDouble()
-                act2 = ""
-            } else {
-                num2 = operands[i + 1].dropLast(1).toDouble()
-                act2 = operands[i + 1].takeLast(1)
-            }
-            when (operands[i].takeLast(1)) {
-                "*" -> {
-                    operands[i + 1] = (num1 * num2).toString() + act2
-                    operands.removeAt(i)
-                    --size
-                    --i
+        fun result(string: String): String {
+            val operands = mutableListOf<String>()
+            if (string.isNotBlank()) {
+                val regex = Regex("""-?\d+(\.\d+)?[*/\-+]?""")
+                regex.findAll(string).forEach {
+                    operands.add(it.value)
                 }
-                "/" -> {
-                    if (num2 == 0.0) {
-                        return "error"
+            }
+            //removing the last symbol of the last operand if action
+            operands[operands.size - 1] = operands.last().replace("""[*/\-+]?$""".toRegex(), "")
+            //perform actions:
+            var i = 0
+            var size = operands.size
+            //first multiply and divide
+            while (i < (size - 1)) {
+                val num1 = operands[i].dropLast(1).toDouble()
+                var num2: Double
+                var act2: String
+                if (i == size - 2) {
+                    num2 = operands[i + 1].toDouble()
+                    act2 = ""
+                } else {
+                    num2 = operands[i + 1].dropLast(1).toDouble()
+                    act2 = operands[i + 1].takeLast(1)
+                }
+                when (operands[i].takeLast(1)) {
+                    "*" -> {
+                        operands[i + 1] = (num1 * num2).toString() + act2
+                        operands.removeAt(i)
+                        --size
+                        --i
                     }
-                    operands[i + 1] = (num1 / num2).toString() + act2
-                    operands.removeAt(i)
-                    --size
-                    --i
+                    "/" -> {
+                        if (num2 == 0.0) {
+                            return "error"
+                        }
+                        operands[i + 1] = (num1 / num2).toString() + act2
+                        operands.removeAt(i)
+                        --size
+                        --i
+                    }
                 }
+                ++i
             }
-            ++i
+            //then add and subtract
+            size = operands.size
+            i = 0
+            while (i < (size - 1)) {
+                val num1 = operands[i].dropLast(1).toDouble()
+                var num2: Double
+                var act2: String
+                if (i == size - 2) {
+                    num2 = operands[i + 1].toDouble()
+                    act2 = ""
+                } else {
+                    num2 = operands[i + 1].dropLast(1).toDouble()
+                    act2 = operands[i + 1].takeLast(1)
+                }
+                when (operands[i].takeLast(1)) {
+                    "+" -> {
+                        operands[i + 1] = (num1 + num2).toString() + act2
+                        operands.removeAt(i)
+                        --size
+                        --i
+                    }
+                    "-" -> {
+                        operands[i + 1] = (num1 - num2).toString() + act2
+                        operands.removeAt(i)
+                        --size
+                        --i
+                    }
+                }
+                ++i
+            }
+            return operands[0]
         }
-        //then add and subtract
-        size = operands.size
-        i = 0
-        while (i < (size - 1)) {
-            val num1 = operands[i].dropLast(1).toDouble()
-            var num2: Double
-            var act2: String
-            if (i == size - 2) {
-                num2 = operands[i + 1].toDouble()
-                act2 = ""
-            } else {
-                num2 = operands[i + 1].dropLast(1).toDouble()
-                act2 = operands[i + 1].takeLast(1)
-            }
-            when (operands[i].takeLast(1)) {
-                "+" -> {
-                    operands[i + 1] = (num1 + num2).toString() + act2
-                    operands.removeAt(i)
-                    --size
-                    --i
-                }
-                "-" -> {
-                    operands[i + 1] = (num1 - num2).toString() + act2
-                    operands.removeAt(i)
-                    --size
-                    --i
-                }
-            }
-            ++i
-        }
-//        ops.forEach { print(it) }
-//        println(" = ${operands[0].toString()}")
-        return operands[0]
     }
-}
 }
